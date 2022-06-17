10f1e580546fd4bfda2bfa17b05ce63903885238
Lucas Lima <lucaslima939@gmail.com>
2022-06-17 11:50:56 -0300

    fix: change material color

--- lib/ui/theme/digital_idea_theme.dart
+++ lib/ui/theme/digital_idea_theme.dart
@@ -1,7 +1,51 @@
 import 'package:flutter/material.dart';
 
 class DigitalIdeaTheme {
-  static get oceanBlue => const Color(0xff5ccbcc);
-  static get basicPink => const Color(0xffd18aa2);
-  static get bulbYellow => const Color(0xffffdb4e);
+  static MaterialColor get oceanBlue => const MaterialColor(
+        0xff5ccbcc,
+        {
+          50: Color(0xff5ccbcc),
+          100: Color(0xff5ccbcc),
+          200: Color(0xff5ccbcc),
+          300: Color(0xff5ccbcc),
+          400: Color(0xff5ccbcc),
+          500: Color(0xff5ccbcc),
+          600: Color(0xff5ccbcc),
+          700: Color(0xff5ccbcc),
+          800: Color(0xff5ccbcc),
+          900: Color(0xff5ccbcc),
+        },
+      );
+
+  static MaterialColor get basicPink => const MaterialColor(
+        0xffd18aa2,
+        {
+          50: Color(0xffd18aa2),
+          100: Color(0xffd18aa2),
+          200: Color(0xffd18aa2),
+          300: Color(0xffd18aa2),
+          400: Color(0xffd18aa2),
+          500: Color(0xffd18aa2),
+          600: Color(0xffd18aa2),
+          700: Color(0xffd18aa2),
+          800: Color(0xffd18aa2),
+          900: Color(0xffd18aa2),
+        },
+      );
+
+  static MaterialColor get bulbYellow => const MaterialColor(
+        0xffffdb4e,
+        {
+          50: Color(0xffffdb4e),
+          100: Color(0xffffdb4e),
+          200: Color(0xffffdb4e),
+          300: Color(0xffffdb4e),
+          400: Color(0xffffdb4e),
+          500: Color(0xffffdb4e),
+          600: Color(0xffffdb4e),
+          700: Color(0xffffdb4e),
+          800: Color(0xffffdb4e),
+          900: Color(0xffffdb4e),
+        },
+      );
 }
